7a5d4185bad90a557d12f5bb1be4fa4337d897e7
kabirsib349 <kabiribrahim349@gmail.com>
2025-01-18 18:07:19 +0100

    Create SugarLevel.java

---
+++ projet_IL/Java_application/src/SugarLevel.java
@@ -0,0 +1,28 @@
+package fr.coffeemocha.modele;
+
+public enum SugarLevel {
+	
+	NO_SUGAR(0),   
+    LOW(1),        
+    MEDIUM(2),     
+    HIGH(3);         
+
+    private final int level;
+    
+    private SugarLevel(int level) {
+		this.level=level;
+	}
+    
+    public int getLevel() {
+        return level;
+    }
+    
+    public static SugarLevel fromLevel(int level) {
+    	for(SugarLevel sugarLevel: SugarLevel.values()) {
+    		if(sugarLevel.level== level)
+    			return sugarLevel;
+    	}
+    	throw new IllegalArgumentException("Niveau de sucre invalide : " + level);
+    }
+    
+}
