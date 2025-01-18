b153a88c3f7523fa93b0d2a1187bed339a2c46f5
kabirsib349 <kabiribrahim349@gmail.com>
2025-01-18 18:04:11 +0100

    Create Order.java

---
+++ projet_IL/Java_application/src/Order.java
@@ -0,0 +1,34 @@
+package fr.coffeemocha.modele;
+
+public class Product {
+
+	private int productId;
+    private String name;
+    private String category;
+    private boolean available;
+    
+    
+    public Product(int productId, String name, String category, boolean available) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.available = available;
+    }
+    
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+}
