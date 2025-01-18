bf8431374730b26db921ba6fa9003b2fda9c847c
kabirsib349 <kabiribrahim349@gmail.com>
2025-01-18 18:08:49 +0100

    Update OrderStatus.java

--- projet_IL/Java_application/src/OrderStatus.java
+++ projet_IL/Java_application/src/OrderStatus.java
@@ -1,34 +1,9 @@
 package fr.coffeemocha.modele;
 
-public class Product {
-
-	private int productId;
-    private String name;
-    private String category;
-    private boolean available;
-    
-    
-    public Product(int productId, String name, String category, boolean available) {
-        this.productId = productId;
-        this.name = name;
-        this.category = category;
-        this.available = available;
-    }
-    
-    public int getProductId() {
-        return productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
+public enum OrderStatus {
+	
+	EN_ATTENTE,    
+    EN_COURS, 
+    PRET,     
 }
+
