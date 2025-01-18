f13a6e0e5a052b270b39b542dfda70be2a09e65f
kabirsib349 <kabiribrahim349@gmail.com>
2025-01-19 00:33:25 +0100

    Update OrderStatus.java

--- projet_IL/Java_application/src/OrderStatus.java
+++ projet_IL/Java_application/src/OrderStatus.java
@@ -2,8 +2,18 @@ package fr.coffeemocha.modele;
 
 public enum OrderStatus {
 	
-    EN_ATTENTE,    
-    EN_COURS, 
-    PRET,     
+	EN_ATTENTE("EN_ATTENTE"),    
+    EN_COURS("EN_COURS"), 
+    PRET("PRET");
+	
+	private final String value;
+	
+	private OrderStatus(String value) {
+		this.value=value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
 }
 
